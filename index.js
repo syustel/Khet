994b1a29a171f765096c57a800d2556f332fa022
Sebastian Yuste <sebastian.yuste@sansano.usm.cl>
2018-10-27 12:09:24 -0300

    handleRequest function with readFile

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+var path = require('path');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', handleRequest);
 
+function handleRequest(req, res){
+    // What did we request?
+  var pathname = req.url;
+  
+  // If blank let's ask for index.html
+  if (pathname == '/') {
+    pathname = '/index.html';
+  }
+  
+  // Ok what's our file extension
+  var ext = path.extname(pathname);
+
+  // Map extension to file type
+  var typeExt = {
+    '.html': 'text/html',
+    '.js':   'text/javascript',
+    '.css':  'text/css'
+  };
+  // What is it?  Default to plain text
+
+  var contentType = typeExt[ext] || 'text/plain';
+
+  // User file system module
+  fs.readFile(__dirname + pathname,
+    // Callback function for reading
+    function (err, data) {
+      // if there is an error
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading ' + pathname);
+      }
+      // Otherwise, send the data, the contents of the file
+      res.writeHead(200,{ 'Content-Type': contentType });
+      res.end(data);
+    }
+  );
+  //res.sendFile(__dirname + '/index.html');
+}
 
 
 io.on('connection', function(socket){
