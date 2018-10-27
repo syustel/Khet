bb9f2def4da202f5c9c6109e9b32142c96533425
Sebastian Yuste <sebastian.yuste@sansano.usm.cl>
2018-10-27 11:53:05 -0300

    socketio test

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('clicked', function(msg){
+    console.log('message: ' + msg);
+    io.emit('todos', msg);
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
